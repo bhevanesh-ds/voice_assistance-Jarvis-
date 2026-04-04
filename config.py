9ef7c43af4ebe48a88a2dea4345a37f9175db654
bhevanesh_ds <bhavaneshnaidu14@gmail.com>
2026-04-04 22:44:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 CITY = "Visakhapatnam"
-WEATHER_API_KEY = "c9195446eaf67072040bc89f58cabe5c"
+WEATHER_API_KEY = "your API_KEY"
 ASSISTANT_NAME = "Jarvis"
 USER_NAME = "Bhevanesh"
 MUSIC_FOLDER = r"C:\Users\bhava\Music"
-NEWS_API_KEY = "c211081b65b44c758e2dadcfe549ab5f"
+NEWS_API_KEY = "your API_KEY"
