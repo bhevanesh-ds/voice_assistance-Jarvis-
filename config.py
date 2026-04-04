d6dcf353b8d37d1cfbcb1107cdf9b734e5c3793e
bhevanesh_ds <bhavaneshnaidu14@gmail.com>
2026-04-04 23:00:38 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-CITY = "Visakhapatnam"
-WEATHER_API_KEY = "your API_KEY"
-ASSISTANT_NAME = "Jarvis"
-USER_NAME = "Bhevanesh"
-MUSIC_FOLDER = r"C:\Users\bhava\Music"
-NEWS_API_KEY = "your API_KEY"
